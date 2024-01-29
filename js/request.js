10640e4a5244db71febe31f8168acced7b053316
teatea-zz <lalahyun.d@gmail.com>
2024-01-29 16:17:10 +0900

    슬라이더 이미지 데이터베이스로 부터 응답 및 교체

--- js/request.js
+++ js/request.js
@@ -43,15 +43,23 @@ async function getRequest(url) {
 // https://www.dabipyeung.com/soaply_backend/model/get_products.php?qnt=3
 async function getProducts(qnt) {
   const url = `https://www.dabipyeung.com/soaply_backend/model/get_products.php?qnt=${qnt}`;
+  const imageBox = document.querySelectorAll('.slider-image');
+  console.log(imageBox);
 
   try {
     // 데이터 요청 및 응답 시도 : 성공일 경우 첫번째 코드 블럭으로 이동
     const data = await getRequest(url);
-    console.log(data);
+    // console.log(data);
+    imageBox.forEach((image, idx) => {
+      // console.log(data[idx].pro_img);
+      const imageEl = `<img src="/soaply/images/${data[idx].pro_img}" alt="" />`;
+      // console.log(imageEl);
+      image.insertAdjacentHTML('beforeend', imageEl);
+    });
   } catch (error) {
     //실패할 경우 두번째 코드 블럭으로 이동
     console.log('Error : ', error);
   }
 }
 
-getProducts(4);
+getProducts(3);
